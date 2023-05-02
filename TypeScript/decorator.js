27fa3ff04d815bdc724d0ad381467962656a450b
yongjulejule <lyjshow200@gmail.com>
2023-05-02 16:09:49 +0900

    :hammer: chore : add typescript decorator

---
+++ TypeScript/decorator.js
@@ -0,0 +1,65 @@
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.push(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.push(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var _this = this;
+function first() {
+    console.log("first(): factory evaluated");
+    return function (target, propertyKey, descriptor) {
+        console.log("first(): called");
+    };
+}
+function second() {
+    console.log("second(): factory evaluated");
+    return function (target, propertyKey, descriptor) {
+        console.log("second(): called");
+    };
+}
+var ExampleClass = function () {
+    var _a;
+    var _instanceExtraInitializers = [];
+    var _method_decorators;
+    return _a = /** @class */ (function () {
+            function ExampleClass() {
+                __runInitializers(this, _instanceExtraInitializers);
+            }
+            ExampleClass.prototype.method = function () { };
+            return ExampleClass;
+        }()),
+        (function () {
+            _method_decorators = [first(), second()];
+            __esDecorate(_a, null, _method_decorators, { kind: "method", name: "method", static: false, private: false, access: { has: function (obj) { return "method" in obj; }, get: function (obj) { return obj.method; } } }, null, _instanceExtraInitializers);
+        })(),
+        _a;
+}();
+ExampleClass.method();
---
+++ TypeScript/decorator.ts
@@ -0,0 +1,22 @@
+function first() {
+  console.log("first(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("first(): called");
+  };
+}
+ 
+function second() {
+  console.log("second(): factory evaluated");
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    console.log("second(): called");
+  };
+}
+ 
+class ExampleClass {
+  @first()
+  @second()
+  method(target, key, disc) : void {}
+}
+
+const a = new ExampleClass();
+a.method(1, 2, 3);
